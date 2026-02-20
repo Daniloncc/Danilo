d2860660b4a3a8aea6487609e610c9e8b37de958
Danilo Costa <dancc86@gmail.com>
2026-02-20 16:31:30 -0500

    Ajouter le composant cursor pour tous les pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProjectDetail from "./pages/ProjectDetail";
+import CustomCursor from "./components/CustomCursorWeb";
+// These pages will be created later
+// import Industrial from "./pages/Industrial"
+// import Dentist from "./pages/Dentist"
+
+export default function App() {
   return (
-    <div>
-      <h1>Mon Portfolio</h1>
-    </div>
+    <BrowserRouter>
+      <CustomCursor />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
+        {/* <Route path="/industrial" element={<Industrial />} /> */}
+        {/* <Route path="/dentist" element={<Dentist />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
