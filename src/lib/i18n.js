347dbeade43de8364e93538084ba88d515f920f7
Danilo Costa <dancc86@gmail.com>
2026-02-24 16:57:11 -0500

    Ajoute de la bibliotheque i18n pour faire un site multilangue. Creation d'un dossier avec les fichers en anglais, francais, espagnol et portugais avec tous le variables pour la partie web

---
+++ src/lib/i18n.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import fr from "../locales/fr/translation.json";
+import en from "../locales/en/translation.json";
+import pt from "../locales/pt/translation.json";
+import es from "../locales/es/translation.json";
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            fr: { translation: fr },
+            en: { translation: en },
+            pt: { translation: pt },
+            es: { translation: es },
+        },
+        fallbackLng: "en",
+        interpolation: { escapeValue: false },
+    });
+
+export default i18n;
